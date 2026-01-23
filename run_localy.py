920a34791d5fb48bb14345c79a8daf234908ca06
Duarte Barbosa <duarteasbarbosa@gmail.com>
2026-01-23 14:05:08 +0000

    update run local script with the new format of img loader

--- run_localy.py
+++ run_localy.py
@@ -2,15 +2,13 @@ from loaders.image_loader import create_img_loader, ImgLoaderTypes
 from loaders.model_loader import ModelLoader
 from utils.stdin_loader import read_stdin
 
-
-
 if __name__ == "__main__":
-    img_path=read_stdin()
+    img_path = read_stdin()
     if len(img_path) == 0:
         raise ValueError("Image path cannot be empty")
-        
-    ocv_loader = create_img_loader(ImgLoaderTypes.OcvLoader)
-    img_normalized = ocv_loader.load_and_normalize(img_path=img_path)
+
+    ocv_loader = create_img_loader(ImgLoaderTypes.OcvImageLoader)
+    img_normalized = ocv_loader.load_local_img(img_path=img_path, height=224, width=224)
 
     model_loader = ModelLoader()
     model_response = model_loader.run_prediction(input_data=img_normalized)
